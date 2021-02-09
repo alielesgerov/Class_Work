273f528db18eaf6a3d6fa3924d0f01a707ec0257
Voregsele <noid379@gmail.com>
2021-02-09 17:53:35 +0400

    3rd Commit

--- Class_Work/Clases.cs
+++ Class_Work/Clases.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Class_Work
 {
-    class Clases
+    class CEO : Human , IWorkers
     {
+        public string Position { get; set; }
+        public double Salary { get; set; }
+    }
+
+    class Worker : Human, IWorkers
+    {
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public string StartTime { set; get; }
+        public string EndTime { set; get; }
+        public Worker(string name, string surname, int age, string position, double salary, string startTime, string endTime)
+        {
+            _guid =new Guid();
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Position = position;
+            Salary = salary;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+    }
+
+    class Operation:Empty
+    {
+        string ProcessName { set; get; }
+        
     }
 }
