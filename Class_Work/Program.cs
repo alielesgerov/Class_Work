1d10c1a15849aa8099c4d847f3fd3c1090b74e48
Voregsele <noid379@gmail.com>
2021-02-09 17:28:25 +0400

    first commit

---
+++ Class_Work/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Work
+{
+    abstract class Empty
+    {
+        public abstract Guid NewGuid();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
